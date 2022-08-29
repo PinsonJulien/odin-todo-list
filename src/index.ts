32d9f0f51cda2a20c500a9cae275e4ba62c2bf36
Julien Pinson <pinsonj01@gmail.com>
2022-08-29 11:37:08 +0200

    Setting up the base layout

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 // CSS styles.
 import '../src/css/main.scss';
+import Layout from './layout/layout';
+
+const layout = new Layout();
+
+document.body.appendChild(layout.getRoot());
--- src/layout/layout.ts
+++ src/layout/layout.ts
@@ -2,28 +2,51 @@ import Component from "../components/component";
 
 export default class Layout extends Component {
   private readonly header = document.createElement('header');
+  private readonly aside = document.createElement('aside');
   private readonly main = document.createElement('main');
   private readonly footer = document.createElement('footer');
   
   constructor () {
     super();
     this.root.setAttribute('id', 'layout');
-    this.header.setAttribute('id', 'layout-header');
-    this.main.setAttribute('id', 'layout-main');
-    this.footer.setAttribute('id', 'layout-footer');
 
     this.root.append(
       this.header,
+      this.aside,
       this.main,
       this.footer
     );
+    
+    // Header
+    const leftHeader = document.createElement('div');
+    const rightHeader = document.createElement('div');
+
     const brand = document.createElement('p');
     brand.textContent = "Odin - Todo List";
-    
-    this.header.append(
-      brand,
+
+    const burgerBtn = document.createElement('button');
+    burgerBtn.textContent = "burger";
+    burgerBtn.addEventListener('click', () => {
+      this.aside.classList.toggle('active');
+    });
+
+    leftHeader.append(
+      brand
+    );
+
+    rightHeader.append(
+      burgerBtn
     );
     
+    this.header.append(
+      leftHeader,
+      rightHeader
+    );
+
+    // Aside
+    this.aside.textContent = "test"
+    
+    // Footer
     const pFooter = document.createElement('p');
     pFooter.textContent = "Created by Julien Pinson for The Odin Project, 2022 ";
     this.footer.append(pFooter);
